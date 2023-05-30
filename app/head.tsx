c05ce578fa7f6a6b4cb10ef6f95d4416658da8f7
Ahmed <balochsaeed888@gmail.com>
2023-05-31 01:18:49 +0500

    portfolio ready

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+	return (
+		<>
+			<title>Ahmed Baloch</title>
+			<meta content='width=device-width, initial-scale=1' name='viewport' />
+			<link rel='icon' href='/favicon.ico' />
+		</>
+	);
+}
